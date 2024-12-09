6992cd1042ca1e97153f7ab3d50c774fc26520b1
el peque <mauro-pavesse@hotmail.com>
2024-12-09 18:58:40 -0300

    Envio de email

--- app/src/main/java/com/pequesystems/gestionganadera/ui/AuthActivity.java
+++ app/src/main/java/com/pequesystems/gestionganadera/ui/AuthActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -184,6 +185,24 @@ public class AuthActivity extends AppCompatActivity {
     private void sendPasswordResetEmail(String email) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
 
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(AuthActivity.this, "Por favor ingrese su correo electrónico", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(AuthActivity.this, "Correo de recuperación enviado", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AuthActivity.this, "Error al enviar el correo: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+/*
+    private void sendPasswordResetEmail(String email) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
         // Verificar si el email está registrado en Firebase
         auth.fetchSignInMethodsForEmail(email)
                 .addOnCompleteListener(task -> {
@@ -211,6 +230,8 @@ public class AuthActivity extends AppCompatActivity {
                     }
                 });
     }
+    */
+
 
 
     private void createUserInFirestore(FirebaseUser firebaseUser) {
