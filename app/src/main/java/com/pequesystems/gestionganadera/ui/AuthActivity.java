cbd09be6ae776d24c8ae578ab4c9306eda0c6e87
el peque <mauro-pavesse@hotmail.com>
2025-02-05 19:23:27 -0300

    Envio de mail y guardar login

--- app/src/main/java/com/pequesystems/gestionganadera/ui/AuthActivity.java
+++ app/src/main/java/com/pequesystems/gestionganadera/ui/AuthActivity.java
@@ -1,12 +1,14 @@
 package com.pequesystems.gestionganadera.ui;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +50,13 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
 
         setTitle("Autenticación");
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            // El usuario ya está autenticado, redirige a la pantalla principal
+            showHome();
+        }
 
         auth_editText_email = findViewById(R.id.auth_editText_email);
         auth_editText_password = findViewById(R.id.auth_editText_password);
@@ -190,10 +199,23 @@ public class AuthActivity extends AppCompatActivity {
             return;
         }
 
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(AuthActivity.this, "Ingrese un correo electrónico válido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Enviando correo de recuperación...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
         auth.sendPasswordResetEmail(email)
                 .addOnCompleteListener(task -> {
+                    progressDialog.dismiss();
                     if (task.isSuccessful()) {
                         Toast.makeText(AuthActivity.this, "Correo de recuperación enviado", Toast.LENGTH_SHORT).show();
+                    } else if (task.getException() instanceof FirebaseAuthInvalidUserException) {
+                        Toast.makeText(AuthActivity.this, "El correo no está registrado", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(AuthActivity.this, "Error al enviar el correo: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
